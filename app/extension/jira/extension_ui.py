093849224d7206cdf7964a66b6074af58ffb763e
sighphyre <liquidwicked64@gmail.com>
2022-02-07 15:11:55 +0200

    Patches for unleash

--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -4,41 +4,30 @@ from selenium.webdriver.common.by import By
 
 from selenium_ui.base_page import BasePage
 from selenium_ui.conftest import print_timing
-from selenium_ui.jira.pages.pages import Login
+from selenium_ui.jira.pages.pages import Login, Issue
 from util.conf import JIRA_SETTINGS
+from flask import Flask, jsonify
+from threading import Thread
+from uuid import uuid4
+import sys
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 
 def app_specific_action(webdriver, datasets):
+
+    unleashed_issues = ["AANES-417", "AANES-348", "AANES-271"]
+
     page = BasePage(webdriver)
-    if datasets['custom_issues']:
-        issue_key = datasets['custom_issue_key']
+    eprint("Preloading the issue test")
 
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
+    issue_page = Issue(webdriver, issue_key=random.choice(unleashed_issues))
 
-    @print_timing("selenium_app_custom_action")
+    @print_timing("selenium_app_add_toggle")
     def measure():
-        @print_timing("selenium_app_custom_action:view_issue")
-        def sub_measure():
-            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
-            page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.ID, "ID_OF_YOUR_APP_SPECIFIC_UI_ELEMENT"))  # Wait for you app-specific UI element by ID selector
-        sub_measure()
+        issue_page.go_to()
+        page.wait_until_visible((By.CLASS_NAME, "toggle-status-container"))
+
     measure()
 
